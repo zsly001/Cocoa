f864ae33197449c1448a67c98584248060fba14c
liuli17 <liuli17@staff.weibo.com>
2023-07-19 00:26:06 +0800

    union

--- cocoa-algo/src/main/java/com/cocoa/algo/union/Main.java
+++ cocoa-algo/src/main/java/com/cocoa/algo/union/Main.java
@@ -6,13 +6,21 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<String> values = Arrays.asList("a","b","c","d","e","f","g");
+        /*List<String> values = Arrays.asList("a","b","c","d","e","f","g");
         UnionSet<String> us = new UnionSet<>(values);
         System.out.println("a isSame e = "+us.isSameSet("a","e"));
 
         us.union("a","b");
         us.union("b","c");
-        System.out.println("a isSame c = "+us.isSameSet("a","c"));
+        System.out.println("a isSame c = "+us.isSameSet("a","c"));*/
+
+        UnionInt un = new UnionInt(10);
+        un.union(0,3);
+        un.union(5,7);
+        un.union(5,9);
+        System.out.println("count: "+un.getCount());
+
+
     }
 
 
